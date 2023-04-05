ef514d87cc0df1a07fde95f0455d5afa1d982edb
Danvace <vipdan055@gmail.com>
2023-04-05 20:15:00 +0300

    Revert "regenerate to Maven"
    This reverts commit 6fb0847b8f5bc2204c364b47cf9e423bcd990b0a.

--- src/ua/lviv/iot/algo/part1/lab1/InsectManager.java
+++ src/ua/lviv/iot/algo/part1/lab1/InsectManager.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class InsectManager extends ua.lviv.iot.algo.part1.lab1.AddInsect {
+public class InsectManager extends AddInsect{
 
     private static final List<Insect> insects = new ArrayList<>();
 
